6ca0c6bcf28a46df41c304981de405422b1d22d1
Vanessa Helena <vanesa-lima10@hotmail.com>
2020-10-22 18:13:52 -0300

    Finalizando as autenticaçoes

---
+++ primeiro-projeto-node/src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,44 @@
+import { Request, Response, NextFunction } from 'express';
+import { verify } from 'jsonwebtoken';
+
+import authConfig from '../config/auth';
+
+interface TokenPayload {
+  iat: number;
+  exp: number;
+  sub: string;
+}
+
+export default function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction,
+): void {
+  // validação do token JWT
+
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    throw new Error('JWT token in missing');
+  }
+
+  // Bearer sauidhasudh
+
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const decoded = verify(token, authConfig.jwt.secret);
+
+    const { sub } = decoded as TokenPayload;
+
+    request.user = {
+      id: sub,
+    };
+
+    console.log(decoded);
+
+    return next();
+  } catch {
+    throw new Error('Invalid JWT token');
+  }
+}
