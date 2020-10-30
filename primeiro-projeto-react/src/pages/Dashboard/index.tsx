ff7c133f9193422f042ee136dc28d92dc72e309a
Vanessa Helena <vanesa-lima10@hotmail.com>
2020-10-30 16:49:32 -0300

    Finalizando a pagina de pesquisa

--- primeiro-projeto-react/src/pages/Dashboard/index.tsx
+++ primeiro-projeto-react/src/pages/Dashboard/index.tsx
@@ -1,58 +1,98 @@
-import React from 'react';
+import React, { useState, useEffect, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg';
 
-import { Title, Form, Repositories } from './styled';
+import { Title, Form, Repositories, Error } from './styles';
+
+interface Repository {
+  full_Name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
 
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [inputError, setInputError] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>(() => {
+    const storangedRepositories = localStorage.getItem(
+      '@GithubExplorer:repositories',
+    );
+
+    if (storangedRepositories) {
+      return JSON.parse(storangedRepositories);
+    }
+    return [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem(
+      '@GithubExplorer:repositories',
+      JSON.stringify(repositories),
+    );
+  }, [repositories]);
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+    // adição de um novo repositorio
+    // Consumir API do Guithub
+    // Salvar novo repositorio no estado
+
+    if (!newRepo) {
+      setInputError('Digite o autor/nome do repositório');
+      return;
+    }
+
+    try {
+      const response = await api.get<Repository>(`repos/${newRepo}`);
+
+      const repository = response.data;
+
+      setRepositories([...repositories, repository]);
+      setNewRepo('');
+      setInputError('');
+    } catch (err) {
+      setInputError('Erro na busca por esse repositório');
+    }
+  }
+
   return (
     <>
       <img src={logoImg} alt="Github Explorer" />
       <Title>explore Repositórios no Guithub</Title>
 
-      <Form action="">
-        <input placeholder="Digite o nome do repoitório" />
+      <Form hasError={!!inputError} onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={e => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do repositório"
+        />
         <button type="submit">Pesquisar</button>
       </Form>
 
+      {inputError && <Error>{inputError}</Error>}
+
       <Repositories>
-        <a href="teste">
-          <img
-            src="https://avatars3.githubusercontent.com/u/58368539?s=460&u=c8307a31c2880c022f3343df37257ad871620f47&v=4"
-            alt="Vanessa Helena"
-          />
-          <div>
-            <strong>Central/Biplan</strong>
-            <p>Easy peasy highly React</p>
-          </div>
+        {repositories.map(repository => (
+          <a key={repository.full_Name} href="teste">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_Name}</strong>
+              <p>{repository.description}</p>
+            </div>
 
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img
-            src="https://avatars3.githubusercontent.com/u/58368539?s=460&u=c8307a31c2880c022f3343df37257ad871620f47&v=4"
-            alt="Vanessa Helena"
-          />
-          <div>
-            <strong>Central/Biplan</strong>
-            <p>Easy peasy highly React</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img
-            src="https://avatars3.githubusercontent.com/u/58368539?s=460&u=c8307a31c2880c022f3343df37257ad871620f47&v=4"
-            alt="Vanessa Helena"
-          />
-          <div>
-            <strong>Central/Biplan</strong>
-            <p>Easy peasy highly React</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
     </>
   );
