74e5ec75a2f7ccec5ce74dbb6681eaa80889cd5f
Vanessa Helena <vanesa-lima10@hotmail.com>
2020-11-04 18:42:58 -0300

    Iniciando a pagina de repositorio

--- primeiro-projeto-react/src/pages/Repository/index.tsx
+++ primeiro-projeto-react/src/pages/Repository/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronsLeft } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Header } from './styles';
 
 interface RepositoryParams {
   repository: string;
@@ -8,7 +13,27 @@ interface RepositoryParams {
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
 
-  return <h1>Repository:{params.repository}</h1>;
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="Github Explorer" />
+        <Link to="/dashboard">
+          <FiChevronsLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+
+        </header>
+        <ul>
+
+            </ul>
+
+      </RepositoryInfo>
+    </>
+  );
 };
 
 export default Repository;
